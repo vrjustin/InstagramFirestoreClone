c57b6783960d7a8c4fc6adcda216463c76b1f469
Justin Maronde <myemailisjustin@me.com>
2022-09-25 06:19:27 -0400

    * add in a new MainTabController we can set as the rootviewcontroller of the app. Modify the scene delegate to make the scene point to the new MainTabController.

---
+++ InstagramFirestoreClone/Controller/MainTabController.swift
@@ -0,0 +1,18 @@
+//
+//  MainTabController.swift
+//  InstagramFirestoreClone
+//
+//  Created by Justin Maronde on 9/25/22.
+//
+
+import Foundation
+import UIKit
+
+class MainTabController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.red
+    }
+    
+}
