cf1b1faaea61056543c9faa6b2bc6a1f8643eeb6
Justin Maronde <myemailisjustin@me.com>
2022-09-25 10:36:21 -0400

    * change our rootviewcontroller to the new LoginController * build out the LoginController * placeholder for the RegistrationController.

---
+++ InstagramFirestoreClone/Controller/Authentication/RegistrationController.swift
@@ -0,0 +1,21 @@
+//
+//  RegistrationController.swift
+//  InstagramFirestoreClone
+//
+//  Created by Justin Maronde on 9/25/22.
+//
+
+import Foundation
+import UIKit
+
+class RegistrationController: UIViewController {
+    
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .systemPurple
+    }
+}
