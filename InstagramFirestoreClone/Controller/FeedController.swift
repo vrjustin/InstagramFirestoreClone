e8f140c7a9e7875d950989fa9ece85f1e396eb84
Justin Maronde <myemailisjustin@me.com>
2022-09-25 07:07:22 -0400

    * add a UICollectionViewFlowLayout to the FeedController which now inherits from UICollectionViewController * setup the collectionView data sources (hardcoded for now) * setup the collectionView delegates (for hardcoded size for now).

--- InstagramFirestoreClone/Controller/FeedController.swift
+++ InstagramFirestoreClone/Controller/FeedController.swift
@@ -8,10 +8,47 @@
 import Foundation
 import UIKit
 
-class FeedController: UIViewController {
+private let reuseIdentifier = "Cell"
+
+class FeedController: UICollectionViewController {
+    
+    // MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemGray2
+        self.configureUI()
+    }
+    
+    // MARK: - Helpers
+    
+    func configureUI() {
+        collectionView.backgroundColor = .white
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+    }
+    
+}
+
+// MARK: - UICollectionViewDataSource
+
+extension FeedController {
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+        cell.backgroundColor = .red
+        
+        return cell
+    }
+    
+}
+
+// MARK: - UICollectionViewDelegate
+
+extension FeedController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 200)
     }
 }
--- InstagramFirestoreClone/Controller/MainTabController.swift
+++ InstagramFirestoreClone/Controller/MainTabController.swift
@@ -22,7 +22,9 @@ class MainTabController: UITabBarController {
     func configureViewControllers() {
         view.backgroundColor = .white
 
-        let feed = templateNavigationController(unselectedImage: UIImage(imageLiteralResourceName: "home_unselected"), selectedImage: UIImage(imageLiteralResourceName: "home_selected"), rootViewController: FeedController())
+        let layout = UICollectionViewFlowLayout()
+        let feed = templateNavigationController(unselectedImage: UIImage(imageLiteralResourceName: "home_unselected"), selectedImage: UIImage(imageLiteralResourceName: "home_selected"), rootViewController: FeedController(collectionViewLayout: layout))
+        
         let search = templateNavigationController(unselectedImage: UIImage(imageLiteralResourceName: "search_unselected"), selectedImage: UIImage(imageLiteralResourceName: "search_selected"), rootViewController: SearchController())
         let imageSelector = templateNavigationController(unselectedImage: UIImage(imageLiteralResourceName: "plus_unselected"), selectedImage: UIImage(imageLiteralResourceName: "plus_unselected"), rootViewController: ImageSelectorController())
         let notification = templateNavigationController(unselectedImage: UIImage(imageLiteralResourceName: "like_unselected"), selectedImage: UIImage(imageLiteralResourceName: "like_selected"), rootViewController: NotificationController())
